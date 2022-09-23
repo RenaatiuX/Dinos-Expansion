8789bb305716b90ce5635541057d1d95a80f974e
RenaatiuX <77760642+RenaatiuX@users.noreply.github.com>
2022-09-23 19:27:52 +0200

    Initial commit

---
+++ src/main/java/com/rena/dinosexpansion/DinosExpansion.java
@@ -0,0 +1,44 @@
+package com.rena.dinosexpansion;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import software.bernie.geckolib3.GeckoLib;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(DinosExpansion.MOD_ID)
+public class DinosExpansion
+{
+    public static final String MOD_ID = "dinosexpansion";
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public DinosExpansion() {
+
+        GeckoLib.initialize();
+
+        // Register the setup method for modloading
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::setup);
+        // Register the doClientStuff method for modloading
+        bus.addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+}
