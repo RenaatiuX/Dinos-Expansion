481f19f41fbe811d7c8e6c7fac8e23addab55232
RenaatiuX <77760642+RenaatiuX@users.noreply.github.com>
2022-10-08 23:55:13 +0200

    fixed entity

--- src/main/java/com/rena/dinosexpansion/common/entity/aquatic/Parapuzosia.java
+++ src/main/java/com/rena/dinosexpansion/common/entity/aquatic/Parapuzosia.java
@@ -1,11 +1,10 @@
 package com.rena.dinosexpansion.common.entity.aquatic;
 
+import com.rena.dinosexpansion.common.entity.ia.DinosaurAISwimBottom;
 import com.rena.dinosexpansion.common.entity.ia.movecontroller.AquaticMoveController;
 import com.rena.dinosexpansion.core.init.EntityInit;
 import net.minecraft.client.renderer.entity.FishRenderer;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.*;
@@ -14,7 +13,12 @@ import net.minecraft.entity.passive.WaterMobEntity;
 import net.minecraft.entity.passive.fish.PufferfishEntity;
 import net.minecraft.entity.passive.fish.SalmonEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.world.World;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.pathfinding.PathNavigator;
+import net.minecraft.pathfinding.SwimmerPathNavigator;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.*;
 import net.minecraftforge.event.world.NoteBlockEvent;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -23,6 +27,8 @@ import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
+
+import javax.annotation.Nullable;
 
 public class Parapuzosia extends WaterMobEntity implements IAnimatable {
 
@@ -47,23 +53,63 @@ public class Parapuzosia extends WaterMobEntity implements IAnimatable {
     protected void registerGoals() {
         super.registerGoals();
         this.goalSelector.addGoal(0, new FindWaterGoal(this));
-        this.goalSelector.addGoal(0, new AvoidEntityGoal(this, GuardianEntity.class, 8.0F, 1.0D, 1.0D));
-        this.goalSelector.addGoal(0, new AvoidEntityGoal(this, PlayerEntity.class, 8.0F, 1.0D, 1.2D));
-        this.goalSelector.addGoal(4, new RandomSwimmingGoal(this, 1f, 10));
+        this.goalSelector.addGoal(0, new AvoidEntityGoal<>(this, GuardianEntity.class, 8.0F, 1.0D, 1.0D));
+        this.goalSelector.addGoal(0, new AvoidEntityGoal<>(this, PlayerEntity.class, 8.0F, 1.0D, 1.2D));
+        this.goalSelector.addGoal(3, new DinosaurAISwimBottom(this, 0.8F, 7));
+        this.goalSelector.addGoal(4, new RandomSwimmingGoal(this, 0.8F, 3));
+        this.goalSelector.addGoal(5, new LookRandomlyGoal(this));
+        this.goalSelector.addGoal(6, new FollowBoatGoal(this));
         this.goalSelector.addGoal(6, new MeleeAttackGoal(this, 1.2f, true));
         this.goalSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, SalmonEntity.class, true));
     }
 
     private PlayState predicate(AnimationEvent<Parapuzosia> event){
-        if (event.isMoving()){
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.Parapuzosia.Swim"));
+        if (isInWater()){
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.Parapuzosia.Swim", true));
+            return PlayState.CONTINUE;
         }
         return PlayState.CONTINUE;
     }
 
+    private void doInitialPosing(IWorld world) {
+        BlockPos down = this.getPosition();
+        while(!world.getFluidState(down).isEmpty() && down.getY() > 1){
+            down = down.down();
+        }
+        this.setPosition(down.getX() + 0.5F, down.getY() + 1, down.getZ() + 0.5F);
+    }
+
+    @Nullable
     @Override
-    public boolean hasNoGravity() {
-        return this.isInWater();
+    public ILivingEntityData onInitialSpawn(IServerWorld worldIn, DifficultyInstance difficultyIn, SpawnReason reason, @Nullable ILivingEntityData spawnDataIn, @Nullable CompoundNBT dataTag) {
+        if (reason == SpawnReason.NATURAL) {
+            doInitialPosing(worldIn);
+        }
+        return super.onInitialSpawn(worldIn, difficultyIn, reason, spawnDataIn, dataTag);
+    }
+
+    @Override
+    public boolean isNotColliding(IWorldReader worldIn) {
+        return worldIn.checkNoEntityCollision(this);
+    }
+
+    @Override
+    protected PathNavigator createNavigator(World worldIn) {
+        return new SwimmerPathNavigator(this, worldIn);
+    }
+
+    @Override
+    public void travel(Vector3d travelVector) {
+        if (this.isServerWorld() && this.isInWater()) {
+            this.moveRelative(this.getAIMoveSpeed(), travelVector);
+            this.move(MoverType.SELF, this.getMotion());
+            this.setMotion(this.getMotion().mul(0.9D, 0.6D, 0.9D));
+            if (this.getAttackTarget() == null) {
+                this.setMotion(this.getMotion().add(0.0D, -0.005D, 0.0D));
+            }
+        } else {
+            super.travel(travelVector);
+        }
     }
 
     @Override
---
+++ src/main/java/com/rena/dinosexpansion/common/entity/ia/DinosaurAISwimBottom.java
@@ -0,0 +1,43 @@
+package com.rena.dinosexpansion.common.entity.ia;
+
+import net.minecraft.entity.CreatureEntity;
+import net.minecraft.entity.ai.RandomPositionGenerator;
+import net.minecraft.entity.ai.goal.RandomWalkingGoal;
+import net.minecraft.pathfinding.PathType;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
+
+import javax.annotation.Nullable;
+
+public class DinosaurAISwimBottom extends RandomWalkingGoal {
+
+    public DinosaurAISwimBottom(CreatureEntity creatureIn, double speedIn, int chance) {
+        super(creatureIn, speedIn, chance);
+    }
+
+    @Nullable
+    @Override
+    protected Vector3d getPosition() {
+        Vector3d vec = RandomPositionGenerator.findRandomTarget(this.creature, 10, 7);
+
+        for(int var2 = 0; vec != null && !this.creature.world.getBlockState(new BlockPos(vec)).allowsMovement(this.creature.world, new BlockPos(vec), PathType.WATER) && var2++ < 10; vec = RandomPositionGenerator.findRandomTarget(this.creature, 10, 7)) {
+        }
+        int yDrop = 1 + this.creature.getRNG().nextInt(3);
+        if(vec != null){
+            BlockPos pos = new BlockPos(vec);
+            while(this.creature.world.getFluidState(pos).isTagged(FluidTags.WATER) && this.creature.world.getBlockState(pos).allowsMovement(this.creature.world, new BlockPos(vec), PathType.WATER) && pos.getY() > 1){
+                pos = pos.down();
+            }
+            pos = pos.up();
+            int yUp = 0;
+            while(this.creature.world.getFluidState(pos).isTagged(FluidTags.WATER) && this.creature.world.getBlockState(pos).allowsMovement(this.creature.world, new BlockPos(vec), PathType.WATER) && yUp < yDrop){
+                pos = pos.up();
+                yUp++;
+            }
+            return Vector3d.copyCentered(pos);
+        }
+
+        return vec;
+    }
+}
