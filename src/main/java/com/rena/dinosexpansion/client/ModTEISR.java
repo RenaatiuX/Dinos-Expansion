4fe69767f5761d1805e91205ca95659220b0f568
RenaatiuX <77760642+RenaatiuX@users.noreply.github.com>
2022-11-06 16:06:53 +0100

    models

--- src/main/java/com/rena/dinosexpansion/client/ModTEISR.java
+++
@@ -1,8 +0,0 @@
-package com.rena.dinosexpansion.client;
-
-import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
-
-public class ModTEISR extends ItemStackTileEntityRenderer {
-
-
-}
